15a3118126c88418acdf4cd91611ae846695db51
Grafikmartin <grafikmartin@gmail.com>
2025-12-20 16:37:06 +0100

    Add responsive navigation header with logo and hamburger menu

---
+++ src/app/components/header/header.ts
@@ -0,0 +1,22 @@
+import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './header.html',
+  styleUrl: './header.css'
+})
+export class HeaderComponent {
+  isMenuOpen = signal(false);
+
+  toggleMenu() {
+    this.isMenuOpen.update(value => !value);
+  }
+
+  closeMenu() {
+    this.isMenuOpen.set(false);
+  }
+}
+
