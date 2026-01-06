919f9991696b463efa98b306152fe3d5aea2e139
Grafikmartin <grafikmartin@gmail.com>
2026-01-06 17:54:17 +0100

    Add: Impressum und Datenschutz Komponenten mit Routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { ImpressumComponent } from './components/impressum/impressum';
+import { DatenschutzComponent } from './components/datenschutz/datenschutz';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'impressum',
+    component: ImpressumComponent
+  },
+  {
+    path: 'datenschutz',
+    component: DatenschutzComponent
+  }
+];
---
+++ src/app/components/datenschutz/datenschutz.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-datenschutz',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './datenschutz.html',
+  styleUrl: './datenschutz.css'
+})
+export class DatenschutzComponent {
+}
+
